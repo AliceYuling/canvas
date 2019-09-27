d092aee7001a5336e12cf3dd65dae4e9ef9520fe
yuyuling <yuyulingsy@chinamobile.com>
2019-09-27 15:12:36 +0800

    add toolbar

--- index.js
+++ index.js
@@ -5,22 +5,53 @@ var maskCtx = maskCanvas.getContext('2d');
 var eraser = document.getElementById('eraser');
 var pen = document.getElementById('pen');
 var gen = document.getElementById('gen');
+var circle = document.getElementById('circle');
 var isPenSelected = true;
 var isEraserSelected = false;
 var img, isImgLoaded, isIconLoaded, isPainting, startX, startY, curX, curY, prevX, prevY, isClearing;
+var tools = {
+  rect: {
+    isSelected: false,
+    isDrawing: false
+  },
+  circle: {
+    isSelected: false,
+    isDrawing: false
+  },
+  eraser: {
+    isSelected: false,
+    isDrawing: false
+  },
+  pen: {
+    isSelected: true,
+    isDrawing: false
+  },
+  gen: {
+    isSelected: false,
+    isDrawing: false
+  }
+};
+var selectedTool = 'pen';
+const shapeTools = ['rect', 'circle'];
+var rectangle;
 
 loadImage();
 drawImage();
+addToolbarEvents();
 
-eraser.addEventListener('click', function() {
-  isEraserSelected = true;
-  isPenSelected = false;
-});
-
-pen.addEventListener('click', function() {
-  isEraserSelected = false;
-  isPenSelected = true;
-});
+function addToolbarEvents() {
+  let toolEls = document.getElementById('toolbar').getElementsByClassName('toolbar-item');
+  Array.prototype.forEach.call(toolEls, item => {
+    item.addEventListener('click', function(e) {
+      maskCanvas.style.cursor = 'default';
+      tools[selectedTool].isSelected = false;
+      tools[selectedTool].isDrawing = false;
+      tools[item.id].isSelected = true;
+      selectedTool = item.id;
+      if (shapeTools.includes(selectedTool)) maskCanvas.style.cursor = 'crosshair';
+    })
+  });
+}
 
 gen.addEventListener('click', function() {
   oriCtx.drawImage(maskCanvas, 0, 0);
@@ -31,48 +62,72 @@ gen.addEventListener('click', function() {
 });
 
 maskCanvas.onmousedown = function(e) {
-  if (isPenSelected) {
-    isPainting = true;
-    startX = e.pageX - maskCanvas.offsetLeft;
-    startY = e.pageY - maskCanvas.offsetTop;
-    prevX = startX;
-    prevY = startY;
-    maskCtx.beginPath();
-    maskCtx.moveTo(startX, startY);
-    maskCtx.lineWidth = 1;
-    maskCtx.strokeStyle = '#fff';
-  } else {
-    isClearing = true;
+  tools[selectedTool].isDrawing = true;
+  startX = e.pageX - maskCanvas.offsetLeft - 12;
+  startY = e.pageY - maskCanvas.offsetTop - 12;
+  maskCtx.lineWidth = 1;
+  maskCtx.strokeStyle = '#fff';
+  switch (selectedTool) {
+    case 'pen': 
+      prevX = startX;
+      prevY = startY;
+      maskCtx.beginPath();
+      maskCtx.moveTo(startX, startY);
+      break;
+    case 'eraser':
+      break;
+    case 'rect':
+      rectangle = new Path2D();
+      break;
+    default:
+      return;
   }
 };
 
 maskCanvas.onmousemove = function(e) {
-  curX = e.pageX - maskCanvas.offsetLeft;
-  curY = e.pageY - maskCanvas.offsetTop;
-  if (isPainting) {
-    drawPath(curX, curY);
-  } else if (isClearing) {
-    clear(curX, curY);
+  curX = e.pageX - maskCanvas.offsetLeft - 12;
+  curY = e.pageY - maskCanvas.offsetTop - 12;
+  switch(selectedTool) {
+    case 'pen':
+      if (tools['pen'].isDrawing) drawPath(curX, curY);
+      break;
+    case 'eraser':
+      if (tools['eraser'].isDrawing) clear(curX, curY);
+      break;
+    case 'rect':
+      if (tools['rect'].isDrawing) {
+        drawRect(curX, curY);
+      }
+      break;
+    default:
+      return;
   }
 };
 
 maskCanvas.onmouseleave = function(e) {
-  if (isClearing) {
-    isClearing = false;
-  } else {
-    isPainting = false;
-  }
+  tools[selectedTool].isDrawing = false;
 };
 
 maskCanvas.onmouseup = function(e) {
-  if (isPainting) {
-    isPainting = false;
-    maskCtx.closePath();
-    maskCtx.stroke();
-    maskCtx.fillStyle = '#fff';
-    maskCtx.fill();
+  switch (selectedTool) {
+    case 'pen':
+      if (tools['pen'].isDrawing) {
+        tools['pen'].isDrawing = false;
+        maskCtx.closePath();
+        maskCtx.stroke();
+        maskCtx.fillStyle = '#fff';
+        maskCtx.fill();
+      }
+      break;
+    case 'eraser':
+      tools['eraser'].isDrawing = false;
+      break;
+    case 'rect':
+      tools['rect'].isDrawing = false;
+      break;
+    default:
+      break;
   }
-  isClearing = false;
 };
 
 function drawPath(x, y) {
@@ -84,6 +139,12 @@ function drawPath(x, y) {
 
 function clear(x, y) {
   maskCtx.clearRect(x, y, 8, 8);
+}
+
+function drawRect(x, y) {
+  maskCtx.clearRect(0, 0, maskCanvas.width, maskCanvas.height);
+  maskCtx.strokeRect(Math.min(startX, x), Math.min(startY, y), Math.abs(x-startX), Math.abs(y-startY));
+  // maskCtx.stroke(rectangle);
 }
 
 function loadImage() {
