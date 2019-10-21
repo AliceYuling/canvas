c334a773e9f59feb55e36ddedbd03e0652a9a43a
yuyuling <yuyulingsy@chinamobile.com>
2019-10-21 10:14:38 +0800

    添加绘图工具

--- index.js
+++ index.js
@@ -1,24 +1,244 @@
-var stage = new Konva.Stage({
-  container: 'container',
-  width: 500,
-  height: 500
-});
+
+var stage;
+
+var container = document.getElementById('container');
+var eraser = document.getElementById('eraser');
 
 var layer = new Konva.Layer();
 
-stage.add(layer);
+var startPos, lastLine;
 
-var circle = new Konva.Circle({
-  x: stage.width() / 2,
-  y: stage.height() / 2,
-  radius: 70,
-  fill: 'red',
-  stroke: 'black',
-  strokeWidth: 4
-});
+var ellipse, currentNode, tr1, count=0;
 
-circle.draggable('true');
+drawImage();
 
-layer.add(circle);
+var tools = {
+  rect: {
+    isSelected: false,
+    isDrawing: false,
+    isTransform: false
+  },
+  ellipse: {
+    isSelected: false,
+    isDrawing: false,
+    isTransform: false
+  },
+  eraser: {
+    isSelected: false,
+    isDrawing: false,
+    isTransform: false
+  },
+  pen: {
+    isSelected: true,
+    isDrawing: false
+  },
+  gen: {
+    isSelected: false,
+    isDrawing: false
+  }
+};
+var selectedTool = 'pen';
+const shapeTools = ['rect', 'circle'];
 
-layer.draw();
+addToolbarEvents();
+
+function addToolbarEvents() {
+  let toolEls = document.getElementById('toolbar').getElementsByClassName('toolbar-item');
+  Array.prototype.forEach.call(toolEls, item => {
+    item.addEventListener('click', function(e) {
+      container.style.cursor = 'default';
+      tools[selectedTool].isSelected = false;
+      tools[selectedTool].isDrawing = false;
+      tools[selectedTool].isTransform = false;
+      document.getElementById(selectedTool).style.borderColor = '#999';
+      tools[item.id].isSelected = true;
+      selectedTool = item.id;
+      if (shapeTools.includes(selectedTool)) container.cursor = 'crosshair';
+      document.getElementById(selectedTool).style.borderColor = '#87CEFA';
+    })
+  });
+}
+
+eraser.addEventListener('click', function(e) {
+  let nodes = stage.find('.settled');
+  nodes.each(function(node) {
+    node.destroy();
+  });
+  nodes = stage.find('#current');
+  nodes.each(function(node) {
+    node.destroy();
+  });
+  tr1 && tr1.destroy();
+  layer.batchDraw();
+})
+
+function drawImage() {
+  var imageObj = new Image();
+  imageObj.onload = function() {
+    stage = new Konva.Stage({
+      container: 'container',
+      width: imageObj.width,
+      height: imageObj.height
+    });
+    var image = new Konva.Image({
+      x: 0,
+      y: 0,
+      image: imageObj,
+      width: imageObj.width,
+      height: imageObj.height
+    });
+    layer.add(image);
+    layer.batchDraw();
+    stage.add(layer);
+    addMouseEvents();
+  };
+  imageObj.src = './test.jpg';
+}
+
+function addMouseEvents() {
+  stage.on('mousedown touchstart', function(e) {
+    let pos = stage.getPointerPosition();
+    if (!tools[selectedTool].isDrawing) {
+      if (tools[selectedTool].isTransform && selectedTool !== 'pen') {
+        let node = null;
+        if (stage.find('#current').length) {
+          node = stage.find('#current')[0];
+          if (tr1.isTransforming() || node.intersects(pos)) {
+            // 在刚完成绘制的图形内部按下并移动鼠标，不会开始新的绘制，而是拖动图形
+            return;
+          } else {
+            // 否则开始新的绘制，并清除scaling
+            startDrawing(pos);
+          }
+        }
+      }
+      // 新绘制
+      // 可能选择了另一图形工具，此时需要将上一次的Transformer清除，并重置current node
+      else {
+        startDrawing(pos);
+      }
+    }
+  });
+
+  stage.on('mousemove touchmove', function(e) {
+    if (!tools[selectedTool].isDrawing) return;
+    let curPos = stage.getPointerPosition();
+    switch(selectedTool) {
+      case 'pen':
+        freeDraw(curPos.x, curPos.y);
+        break;
+      case 'ellipse':
+        if (tools.ellipse.isTransform) return;
+        drawEllipse(curPos.x, curPos.y);
+        break;
+      case 'rect':
+        if (tools.rect.isTransform) return;
+        drawRect(curPos.x, curPos.y);
+        break;
+      default:
+        break;
+    }
+  })
+
+  stage.on('mouseup touchend', function() {
+    if (tools[selectedTool].isDrawing) {
+      tools[selectedTool].isTransform = true;
+      tools[selectedTool].isDrawing = false;
+      if (selectedTool === 'pen') {
+        lastLine.setAttr('closed', true);
+        layer.batchDraw();
+        tools[selectedTool].isTransform = false;
+      }
+      let node = stage.find('#current')[0];
+      if (node) {
+        tr1 = new Konva.Transformer({
+          node,
+          centeredScaling: true,
+          id: 'tr1'
+        });
+        layer.add(tr1);
+        layer.draw();
+      }
+    }
+  });
+}
+
+function startDrawing(pos) {
+  tr1 && tr1.destroy();
+  node = stage.find('#current')[0];
+  if (node) {
+    node.setAttr('id', `settled${count}`);
+    node.setAttr('name', 'settled');
+    count++;
+  }
+  if (selectedTool === 'pen') {
+    lastLine = new Konva.Line({
+      stroke: 'white',
+      strokeWidth: 5,
+      globalCompositeOperation: 'source-over',
+      points: [pos.x, pos.y]
+    });
+    layer.add(lastLine);
+  }
+  tools[selectedTool].isDrawing = true;
+  tools[selectedTool].isTransform = false;
+  startPos = pos;
+}
+
+function freeDraw(curX, curY) {
+  var newPoints = lastLine.points().concat([curX, curY]);
+  lastLine.points(newPoints);
+  layer.batchDraw();
+}
+
+function drawRect(curX, curY) {
+  const x = startPos.x;
+  const y = startPos.y;
+  const width = Math.abs(curX - x);
+  const height = Math.abs(curY - y);
+  var oldRect = stage.find('.rect');
+  oldRect.forEach(item => item.destroy());
+  rect = new Konva.Rect({
+    x,
+    y,
+    width,
+    height,
+    stroke: 'white',
+    strokeWidth: 4,
+    name: 'rect',
+    draggable: true
+  });
+
+  rect.setAttr('id', 'current');
+
+  layer.add(rect);
+
+  layer.draw();
+}
+
+function drawEllipse(curX, curY) {
+  let startX = startPos.x;
+  let startY = startPos.y;
+  let x = (curX + startX) / 2;
+  let y = (curY + startY) / 2;
+  let radiusX = Math.abs(curX - startX) / 2;
+  let radiusY = Math.abs(curY - startY) / 2;
+  var oldEllipse = stage.find('.ellipse');
+  oldEllipse.forEach(item => item.destroy());
+  ellipse = new Konva.Ellipse({
+    x,
+    y,
+    radiusX,
+    radiusY,
+    stroke: 'white',
+    strokeWidth: 4,
+    name: 'ellipse',
+    draggable: true
+  });
+
+  ellipse.setAttr('id', 'current');
+
+  layer.add(ellipse);
+
+  layer.draw();
+}
